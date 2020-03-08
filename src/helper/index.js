eb9f9435ad07cb4547ccb1dd4b848ad734a987bc
Ian Armour <ianmarmour@gmail.com>
2020-03-08 10:04:56 -0700

    Cleaned up object recursion code for readability

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,43 +1,29 @@
 function iterate(obj, propertyType, propertySet) {
-  const propertyProcessor = (obj, propertyType, propertySet) => {};
-  if (Array.isArray(obj)) {
-    for (let property of obj) {
-      if (obj.hasOwnProperty(property)) {
-        if (typeof obj[property] == "object") {
-          if (
-            propertyType === "key" &&
-            Array.isArray(obj[property]) === false
-          ) {
-            propertySet.add(property);
-          }
-          iterate(obj[property], propertyType, propertySet);
+  const propertyProcessor = (obj, property, propertyType, propertySet) => {
+    if (obj.hasOwnProperty(property)) {
+      if (typeof obj[property] == "object") {
+        if (propertyType === "key" && Array.isArray(obj[property]) === false) {
+          propertySet.add(property);
+        }
+        iterate(obj[property], propertyType, propertySet);
+      } else {
+        if (propertyType === "key") {
+          propertySet.add(property);
         } else {
-          if (propertyType === "key") {
-            propertySet.add(property);
-          } else {
-            propertySet.add(obj[property]);
-          }
+          propertySet.add(obj[property]);
         }
       }
+    }
+  };
+
+  if (Array.isArray(obj)) {
+    for (let property of obj) {
+      propertyProcessor(obj, property, propertyType, propertySet);
     }
   } else {
     for (let property in obj) {
       if (obj.hasOwnProperty(property)) {
-        if (typeof obj[property] == "object") {
-          if (
-            propertyType === "key" &&
-            Array.isArray(obj[property]) === false
-          ) {
-            propertySet.add(property);
-          }
-          iterate(obj[property], propertyType, propertySet);
-        } else {
-          if (propertyType === "key") {
-            propertySet.add(property);
-          } else {
-            propertySet.add(obj[property]);
-          }
-        }
+        propertyProcessor(obj, property, propertyType, propertySet);
       }
     }
   }
